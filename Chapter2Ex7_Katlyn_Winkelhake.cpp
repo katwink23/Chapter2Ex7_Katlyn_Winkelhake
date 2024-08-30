d75aa0375317d33c4f081f8682bdcabd1e456078
Katlyn W. <krw8ateez@gmail.com>
2024-08-30 11:45:00 -0400

    Add project files.

---
+++ Chapter2Ex7_Katlyn_Winkelhake.cpp
@@ -0,0 +1,9 @@
+/*
+Title: Chapter 2 Exercise 7
+File Name: Chapter2Ex7_Katlyn_Winkelhake.cpp
+Date: Aug 30 2024
+Programmer: Katlyn Winkelhake
+Instructions: Make calculations depending upon ocean rising and display the answers
+*/
+
+#include <iostream>
