7894202f5f74fc73085b6eb6ebd5f006ecabbfcf
Katlyn W. <krw8ateez@gmail.com>
2024-08-30 11:48:22 -0400

    update

--- Chapter2Ex7_Katlyn_Winkelhake.cpp
+++ Chapter2Ex7_Katlyn_Winkelhake.cpp
@@ -3,7 +3,29 @@ Title: Chapter 2 Exercise 7
 File Name: Chapter2Ex7_Katlyn_Winkelhake.cpp
 Date: Aug 30 2024
 Programmer: Katlyn Winkelhake
-Instructions: Make calculations depending upon ocean rising and display the answers
+Requirements: Assuming the ocean’s level is currently rising at about 1.5 millimeters per year, write a program that displays:
+
+• The number of millimeters higher than the current level that the ocean’s level will be in 5 years.
+
+• The number of millimeters higher than the current level that the ocean’s level will be in 7 years.
+
+• The number of millimeters higher than the current level that the ocean’s level will be in 10 years.
+
+Once you have coded this and placed in your main branch in GitHub, create a second branch that revises your code so 
+
+that the user can enter the number of the number of millimeters the ocean level rises per year..
 */
 
 #include <iostream>
+
+int main()
+{
+
+
+
+
+
+
+
+	return 0;
+}
